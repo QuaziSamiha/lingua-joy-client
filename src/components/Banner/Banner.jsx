2768f644c84f81fa88336c76237830c96e215b12
QuaziSamiha <1703155@student.ruet.ac.bd>
2024-01-13 11:16:33 -0800

    working on banner

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,15 @@
+import banner from "../../../src/assets/images/banner/coursesBanner.png";
+
+const Banner = () => {
+  return (
+    <div>
+      <div>
+        <div className="flex justify-center">
+          <img src={banner} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/pages/Courses/Courses/Courses.jsx
+++ src/pages/Courses/Courses/Courses.jsx
@@ -1,3 +1,4 @@
+import Banner from "../../../components/Banner/Banner";
 import useCourse from "../../../hooks/useCourse";
 
 function Courses() {
@@ -5,15 +6,16 @@ function Courses() {
   console.log(courses);
   return (
     <>
+      <Banner />
       <section className="border border-red-600 w-[1180px] mx-auto my-24">
         <div className="flex justify-center my-8">
-          <div>
+          <div className="">
             <div className="join">
               <div>
                 <div>
                   <input
-                    className="input input-bordered join-item"
-                    placeholder="Search"
+                    className="input input-bordered join-item w-[500px]"
+                    placeholder="Search by Language..........."
                   />
                 </div>
               </div>
@@ -21,7 +23,9 @@ function Courses() {
                 <span className="indicator-item badge badge-secondary">
                   new
                 </span>
-                <button className="btn join-item">Search</button>
+                <button className="btn join-item bg-[#ba68c8] text-white w-[100px] text-lg">
+                  Search
+                </button>
               </div>
             </div>
           </div>

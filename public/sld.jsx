25c6dfffec52636685bb01925e9d0dc64c292f8b
QuaziSamiha <1703155@student.ruet.ac.bd>
2024-01-16 09:58:30 -0800

    firebase install and configure file done

--- public/sld.jsx
+++
@@ -1,1 +0,0 @@
-// import ins1 from '../src/assets/images/instructor/ins1.jpg'
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDWCbv9f6kiGAQXbbS6-9ajc4AQBBasW6Y",
+  authDomain: "linguajoy-718da.firebaseapp.com",
+  projectId: "linguajoy-718da",
+  storageBucket: "linguajoy-718da.appspot.com",
+  messagingSenderId: "585770075121",
+  appId: "1:585770075121:web:09a3b49747362b74fbfa50",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);

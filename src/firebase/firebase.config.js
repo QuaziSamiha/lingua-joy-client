f7c9e30e58fcab795ba95c24a9826cf6cedcc4c0
QuaziSamiha <1703155@student.ruet.ac.bd>
2024-01-18 08:31:38 -0800

    firbase config to new id

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,13 +1,25 @@
 import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDWCbv9f6kiGAQXbbS6-9ajc4AQBBasW6Y",
+//   authDomain: "linguajoy-718da.firebaseapp.com",
+//   projectId: "linguajoy-718da",
+//   storageBucket: "linguajoy-718da.appspot.com",
+//   messagingSenderId: "585770075121",
+//   appId: "1:585770075121:web:09a3b49747362b74fbfa50",
+// };
+
+// Import the functions you need from the SDKs you need
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDWCbv9f6kiGAQXbbS6-9ajc4AQBBasW6Y",
-  authDomain: "linguajoy-718da.firebaseapp.com",
-  projectId: "linguajoy-718da",
-  storageBucket: "linguajoy-718da.appspot.com",
-  messagingSenderId: "585770075121",
-  appId: "1:585770075121:web:09a3b49747362b74fbfa50",
+  apiKey: "AIzaSyCx5-8lyQkqyk3XDU52UB4aAfqasYXy070",
+  authDomain: "linguajoy-9e391.firebaseapp.com",
+  projectId: "linguajoy-9e391",
+  storageBucket: "linguajoy-9e391.appspot.com",
+  messagingSenderId: "167492980036",
+  appId: "1:167492980036:web:15e21b16e50c2c1e701b11",
 };
 
 // Initialize Firebase

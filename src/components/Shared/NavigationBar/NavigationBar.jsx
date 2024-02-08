4a5b8d30dfcf85ad0991a4e77014c3da0ee58fee
QuaziSamiha <1703155@student.ruet.ac.bd>
2024-02-08 20:24:03 +0600

    navbar done

--- src/components/Shared/NavigationBar/NavigationBar.jsx
+++ src/components/Shared/NavigationBar/NavigationBar.jsx
@@ -25,10 +25,6 @@ const NavigationBar = () => {
       id: "",
       name: "Home",
     },
-    // {
-    //   id: "/aboutus",
-    //   name: "About Us",
-    // },
     { id: "/instructors", name: "Instructors" },
     {
       id: "/courses",
@@ -42,35 +38,37 @@ const NavigationBar = () => {
 
   return (
     <>
-      <div className="navbar bg-[#ba68c8] px-8 font-Outfit">
+      <div className="navbar bg-[#ba68c8] lg:px-8 font-Outfit">
         <div className="navbar-start">
           <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <div tabIndex={0} role="button" className="text-white md:hidden">
               {isOpen ? (
-                <FaBars onClick={() => setIsOpen(false)} />
+                <RxCross2 onClick={() => setIsOpen(false)} />
               ) : (
-                <RxCross2 onClick={() => setIsOpen(true)} />
+                <FaBars onClick={() => setIsOpen(true)} />
               )}
             </div>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+              className={`menu menu-sm dropdown-content mt-6 z-[1] bg-[#ba68c8] p-2 shadow w-[450px] ${
+                isOpen === true ? "" : "hidden"
+              }`}
             >
               {routes.map((route, index) => (
-                <li key={index} className="px-4 uppercase">
+                <li key={index} className="lg:px-4 uppercase text-white">
                   <Link to={route.id}>{route.name}</Link>
                 </li>
               ))}
             </ul>
           </div>
-          <p className="btn btn-ghost text-2xl uppercase font-bold text-white">
+          <p className="pl-4 text-lg lg:text-2xl uppercase font-semibold lg:font-bold text-white">
             LinguaJoy
           </p>
         </div>
-        <div className="navbar-center hidden lg:flex">
+        <div className="navbar-center hidden md:flex">
           <ul className="menu menu-horizontal px-1">
             {routes.map((route, index) => (
-              <li key={index} className="px-1 text-white font-medium">
+              <li key={index} className="lg:px-1 text-white font-medium">
                 <Link
                   className="hover:shadow-lg shadow-[#703e78] uppercase"
                   to={route.id}
@@ -87,7 +85,7 @@ const NavigationBar = () => {
               <img
                 src={user.photoURL}
                 alt=""
-                className="rounded-full w-12 h-12 mr-3"
+                className="rounded-full w-8 lg:w-12 h-8 lg:h-12 mr-3"
               />
             ) : (
               <> </>
@@ -96,14 +94,14 @@ const NavigationBar = () => {
           {user ? (
             <button
               onClick={handleLogOut}
-              className="btn bg-[#dcb3e4] text-lg font-bold text-[#703e78] border-none rounded w-32"
+              className="p-2 lg:p-0 lg:btn bg-[#dcb3e4] text-sm lg:text-lg font-bold text-[#703e78] border-none rounded lg:w-32"
             >
               Sign Out
             </button>
           ) : (
             <Link
               to="/signup"
-              className="btn bg-[#dcb3e4] skeleton text-lg font-bold text-[#703e78] border-none rounded w-32"
+              className="p-2 lg:p-0 lg:btn bg-[#dcb3e4] skeleton text-sm lg:text-lg font-bold text-[#703e78] border-none rounded lg:w-32"
             >
               Join Us
             </Link>

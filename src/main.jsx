90c784cb116b7c349a8d66ca8c4aaa614a27ffe8
QuaziSamiha <1703155@student.ruet.ac.bd>
2025-02-08 12:56:09 +0600

    chaneges

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import { RouterProvider } from "react-router-dom";
 import "aos/dist/aos.css"; // Import AOS styles
 import AOS from "aos";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import router from "./routes/Routes";
 import AuthProvider from "./providers/AuthProvider/AuthProvider";
+import router from "./routes/Routes";
 // main.jsx -- ENTRY POINT OF REACT
 
 // used for animation
